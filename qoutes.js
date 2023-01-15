bdbcd7944536b484d0d151d1c294bfcb9d1ae7b6
flea2003 <121982073+flea2003@users.noreply.github.com>
2023-01-15 23:17:13 +0100

    Add files via upload

---
+++ qoutes.js
@@ -0,0 +1,20 @@
+var up = document.getElementById("nemo");
+
+var safeSet = [
+  "It’s only after you’ve stepped outside your comfort zone that you begin to change, grow, and transform. ― Roy T. Bennett",
+  "Start each day with a positive thought and a grateful heart. ― Roy T. Bennett, The Light in the Heart",
+  "I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain. ― Frank Herbert, Dune",
+  "If you've got nothing to dance about, find a reason to sing. ― Melody Carstairs",
+  "Ever Tried. Ever Failed. No matter. Try again. Fail again. Fail better. ― Samuel Beckett",
+];
+
+
+function random(mn, mx) {
+  return Math.random() * (mx - mn) + mn;
+}
+
+myFunction = () =>{
+    document.getElementById("nemo").innerHTML = safeSet[2];
+}
+
+
